e76186f6f0469dc8e84375b3a9c66155ee5e5b61
Nick <niczz9797@gmail.com>
2020-02-26 18:16:43 +0300

    Функционал работает полностью. ожидание дебага и составление структуры папки, где будет лежать программа

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from tkinter import messagebox as mb
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 def config_read():
@@ -52,33 +53,42 @@ class FrameGen(tk.Frame):
         self.parent.config(menu=menubar)
         filemenu = tk.Menu(menubar, tearoff=0)
         filemenu.add_command(label="Путь к PECHS", command=open_button)
+        filemenu.add_command(label="Reset", command=self.reset)
         filemenu.add_command(label="Exit", command=self.onExit)
-        menubar.add_cascade(label="Файл", menu=filemenu)
 
-    def onExit(self):
-        self.quit()
+        menubar.add_cascade(label="Файл", menu=filemenu)
 
     def notebooks(self):
         nb.add(self, text=f"{self.name}")
         label_name_energy = tk.Label(self, text=f'{self.energy_type}')
-        label_name_energy.grid(row=3, column=0, columnspan=5)
+        label_name_energy.grid(row=4, column=0, columnspan=2)
         label_func = tk.Label(self, text='value')
-        label_func.grid(row=4, column=0, padx=2, pady=10)
+        label_func.grid(row=6, column=0, padx=2, pady=10)
         label_time = tk.Label(self, text='time')
-        label_time.grid(row=4, column=1, padx=2, pady=10)
+        label_time.grid(row=6, column=1, padx=2, pady=10)
 
-        button_browse = tk.Button(self, width=10, text='Load', command=self.ent_load)
-        button_browse.grid(row=1, column=10)
-        button_save = tk.Button(self, width=10, text='Save', command=self.time_save)
-        button_save.grid(row=2, column=10)
-        entry_generate_value = tk.Entry(self, width=6, textvariable=self.cell_numeric)
-        entry_generate_value.grid(row=3, column=11, padx=5)
-        button_generate = tk.Button(self, width=10, text='Generate', command=self.ent)
-        button_generate.grid(row=3, column=10)
-        button_read_gen = tk.Button(self, width=10, text='Read', command=self.get)
-        button_read_gen.grid(row=4, column=10)
+        self.button_browse = tk.Button(self, width=10, text='Load', command=self.ent_load, state='active')
+        self.button_browse.grid(row=1, column=10)
+        self.button_browse_def = tk.Button(self, width=10, text='Load default', command=self.ent_load_def,
+                                           state='active')
+        self.button_browse_def.grid(row=2, column=10)
+        self.button_save = tk.Button(self, width=10, text='Save', command=self.time_save, state='disabled')
+        self.button_save.grid(row=3, column=10)
+        self.button_save_def = tk.Button(self, width=10, text='Save as default', command=self.time_save_def,
+                                         state='disabled')
+        self.button_save_def.grid(row=4, column=10)
+        self.entry_generate_value = tk.Entry(self, width=6, textvariable=self.cell_numeric, state='normal')
+        self.entry_generate_value.grid(row=5, column=11, padx=5)
+        self.button_generate = tk.Button(self, width=10, text='Generate', command=self.ent, state='active')
+        self.button_generate.grid(row=5, column=10)
+        self.button_read_gen = tk.Button(self, width=10, text='Read', command=self.get, state='disabled')
+        self.button_read_gen.grid(row=6, column=10)
+        self.button_calculate = tk.Button(self, width=10, text='Calculate', command=self.calculate, state='disabled')
+        self.button_calculate.grid(row=1, column=30)
 
     def ent(self):
+        self.func_entry_vel.clear()
+        self.time_entry_vel.clear()
         self.func_entry_vel = [tk.StringVar() for _ in range(int(self.cell_numeric.get()))]
         self.time_entry_vel = [tk.StringVar() for _ in range(int(self.cell_numeric.get()))]
         for i in range(int(self.cell_numeric.get())):
@@ -88,12 +98,16 @@ class FrameGen(tk.Frame):
             entry_time.grid(row=5 + i, column=1, padx=10, pady=1)
 
         entry_time = tk.Label(self, width=9, text=f'{self.time_grid()}')
-        entry_time.grid(row=5 + int(self.cell_numeric.get()) + 1, column=1, padx=10, pady=1)
+        entry_time.grid(row=6 + int(self.cell_numeric.get()) + 1, column=1, padx=5, pady=1)
         entry_func = tk.Label(self, width=9, text='[0 : 1]')
-        entry_func.grid(row=5 + int(self.cell_numeric.get()) + 1, column=0, padx=10, pady=1)
+        entry_func.grid(row=6 + int(self.cell_numeric.get()) + 1, column=0, padx=5, pady=1)
+
+        self.button_browse.configure(state='disabled')
+        self.button_browse_def.configure(state='disabled')
+        self.button_read_gen.configure(state='normal')
 
     def ent_load(self):
-        with open(rf'time functions/{self.name}.txt', 'r', encoding='utf-8') as file:
+        with open(rf'time functions/user configuration/{self.name}.txt', 'r', encoding='utf-8') as file:
             lines = file.readlines()
         lines = [line.strip() for line in lines]
         print(lines)
@@ -129,6 +143,53 @@ class FrameGen(tk.Frame):
             mb.showerror('Load error', 'Размерности не совпадают')
             self.onExit()
 
+        self.labes_load_path = tk.Label(self, text=f'time functions/user configuration/{self.name}.txt')
+        self.labes_load_path.grid(row=1, column=11, columnspan=5)
+        self.button_read_gen.configure(state='normal')
+        self.button_generate.configure(state='disabled')
+
+    def ent_load_def(self):
+        with open(rf'time functions/user configuration/default.txt', 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+        lines = [line.strip() for line in lines]
+        print(lines)
+
+        for word in lines[0].split():
+            self.func_entry_vel.append(float(word))
+        for word in lines[1].split():
+            self.time_entry_vel.append(float(word))
+        print('func = ', self.func_entry_vel)
+        print('time = ', self.time_entry_vel)
+
+        entr_utility_func = [tk.StringVar() for _ in range(len(self.func_entry_vel))]
+        entr_utility_time = [tk.StringVar() for _ in range(len(self.func_entry_vel))]
+        for i in range(len(self.func_entry_vel)):
+            entry_func = tk.Entry(self, width=9, justify='center', textvariable=entr_utility_func[i])
+            entry_func.grid(row=5 + i, column=0, padx=10, pady=1)
+            entr_utility_func[i].set('{:.4g}'.format(self.func_entry_vel[i]))
+            self.func_entry_vel[i] = entr_utility_func[i]
+            # print(f'{i} ', type(entr_utility_func[i]), entr_utility_func[i])
+
+        for i in range(len(self.time_entry_vel)):
+            entry_time = tk.Entry(self, width=9, justify='center', textvariable=entr_utility_time[i])
+            entry_time.grid(row=5 + i, column=1, padx=10, pady=1)
+            entr_utility_time[i].set('{:.4g}'.format(self.time_entry_vel[i]))
+            self.time_entry_vel[i] = entr_utility_time[i]
+
+        entry_time = tk.Label(self, width=9, text=f'{self.time_grid()}')
+        entry_time.grid(row=5 + len(self.time_entry_vel) + 1, column=1, padx=10, pady=1)
+        entry_func = tk.Label(self, width=9, text='[0 : 1]')
+        entry_func.grid(row=5 + len(self.func_entry_vel) + 1, column=0, padx=10, pady=1)
+
+        if len(self.func_entry_vel) != len(self.time_entry_vel):
+            mb.showerror('Load error', 'Размерности не совпадают')
+            self.onExit()
+
+        self.labes_load_path = tk.Label(self, text=f'time functions/user configuration/{self.name}.txt')
+        self.labes_load_path.grid(row=1, column=11, columnspan=5)
+        self.button_read_gen.configure(state='normal')
+        self.button_generate.configure(state='disabled')
+
     def get(self):
 
         print('get', type(self.func_entry_vel[0]))
@@ -141,17 +202,120 @@ class FrameGen(tk.Frame):
             self.time_list.append(float(i.get()))
 
         self.value_check(func=self.func_list, time=self.time_list)
+        self.button_generate.configure(state='disabled')
+        self.entry_generate_value.configure(state='disabled')
+        self.button_save.configure(state='active')
+        self.button_save_def.configure(state='active')
+        self.button_browse.configure(state='disabled')
+        self.button_calculate.configure(state='normal')
 
         print('time = ', self.time_list)
         print('func = ', self.func_list)
 
     def time_save(self):
-        with open(rf'time functions/{self.name}.txt', 'w', encoding='utf-8') as file:
+        with open(rf'time functions/user configuration/{self.name}.txt', 'w', encoding='utf-8') as file:
             for i in self.func_list:
                 file.write(f'{i} ')
             file.write('\n')
             for i in self.time_list:
                 file.write(f'{i} ')
+            self.labes_load_path = tk.Label(self, text=f'time functions/user configuration/{self.name}.txt')
+            self.labes_load_path.grid(row=3, column=11, columnspan=5)
+
+    def time_save_def(self):
+        with open(rf'time functions/user configuration/default.txt', 'w', encoding='utf-8') as file:
+            for i in self.func_list:
+                file.write(f'{i} ')
+            file.write('\n')
+            for i in self.time_list:
+                file.write(f'{i} ')
+            self.labes_load_path = tk.Label(self, text=f'time functions/user configuration/default.txt')
+            self.labes_load_path.grid(row=4, column=11, columnspan=5)
+
+    def calculate(self):
+
+        time_cell = self.child_parcecer_grid()
+        print(f'функция {self.func_list}')
+        print(f'время {self.time_list}')
+        entry_f = np.array(self.func_list)
+        entry_t = np.array(self.time_list)
+
+        if entry_t[-1] != time_cell[-1]:
+            entry_t = np.append(entry_t, time_cell[-1])
+            entry_f = np.append(entry_f, 0)
+        if entry_t[0] != 0 and entry_f[0] != 0:
+            entry_t = np.insert(entry_t, 0, 0)
+            entry_f = np.insert(entry_f, 0, 0)
+
+        for i in range(len(entry_t) - 1):
+            if entry_t[i] > entry_t[i + 1]:
+                print(f'Value error время уменьшается на одном из отрезков {i}')
+
+        old_tf = np.loadtxt(r'entry_data\time.tf', skiprows=3)
+
+        time_count = []
+        func_out = []
+        for i in range(len(entry_t) - 1):
+            k, b = Calculations().linear_dif(entry_f[i], entry_t[i], entry_f[i + 1], entry_t[i + 1])
+            # print(f'k = {k} , b = {b}')
+            # print(np.extract((time_cell == entry_t[i]),time_cell))
+            dt = time_cell[1] - time_cell[0]
+            left_side = np.where(time_cell == entry_t[i])[0]
+            right_side = np.where(time_cell == entry_t[i + 1])[0]
+
+            if len(left_side) != 1:
+                left_side = np.where(abs(time_cell - entry_t[i]) <= dt / 2)[0]
+            if len(right_side) != 1:
+                right_side = np.where(abs(time_cell - entry_t[i + 1]) <= dt / 2)[0]
+
+            # print(type(left_side), time_cell[left_side])
+            # print(type(right_side), time_cell[right_side][0], time_cell[100])
+            print(f'{time_cell[left_side]} - {time_cell[right_side]}')
+            if i != len(entry_t) - 2:
+
+                for j in time_cell[left_side[0]:right_side[0]]:
+                    if j == time_cell[right_side[0]]:
+                        print(j)
+                        print(f'curent f len = {len(func_out)}')
+                        print('')
+                    func_out.append(Calculations().solve(j, k, b))
+                    time_count.append(j)
+            else:
+                for j in time_cell[left_side[0]:right_side[0] + 1]:
+                    func_out.append(Calculations().solve(j, k, b))
+                    time_count.append(j)
+
+        func_out = np.array(func_out)
+        time_count = np.array(time_count)
+        output_matrix = np.column_stack((time_count, func_out))
+        print(func_out.shape)
+        print('заданныйэ = ', time_cell.shape, time_cell[-1])
+        print('по факту = ', time_count.shape, time_count[-1])
+        print(old_tf.shape)
+
+        np.savetxt(f'time functions/time_{self.name}.tf', output_matrix, fmt='%-8.4g',
+                   header=f'1 pechs\n{time_count[0]} {time_count[-1]} {1.0}\n{len(time_count)}\n', delimiter='\t',
+                   comments='')
+
+        figure = plt.Figure(figsize=(6, 4), dpi=100)
+        ax = figure.add_subplot(111)
+        ax.plot(time_count, func_out * np.max(old_tf[:, 1]), label='Пользовательская функция')
+        ax.plot(time_count, old_tf[:, 1], label='Стандартная функция')
+        ax.set_xlabel('Time , s', fontsize=14)
+        ax.set_ylabel('Function', fontsize=14)
+        chart_type = FigureCanvasTkAgg(figure, self)
+
+        chart_type.get_tk_widget().grid(row=4, rowspan=100, column=20, columnspan=100)
+        ax.set_title(f'{self.name}')
+        ax.legend()
+
+    def reset(self):
+        nb.destroy()
+        main()
+
+    def onExit(self):
+        self.quit()
+
 
     def time_grid(self):
         a = DataParcer('entry_data/KUVSH.GRD').grid_parcer()
@@ -163,10 +327,22 @@ class FrameGen(tk.Frame):
     def value_check(self, func, time):
         for item in func:
             if not (0 <= item <= 1):
-                mb.showerror('Value error', 'Значение функции выходит за пределы')
+                mb.showerror('Value error', f'Значение функции {item} выходит за пределы')
         for item in time:
             if not (self.child_parcecer_grid()[0] <= item <= self.child_parcecer_grid()[-1]):
-                mb.showerror('Value error', 'Временная функция выходит за пределы')
+                mb.showerror('Value error', f'Значение временной функции {item} выходит за пределы')
+
+
+class Calculations:
+
+    def linear_dif(self, y1, x1, y2, x2):
+        self.k = (y2 - y1) / (x2 - x1)
+        self.b = y1 - (y2 - y1) * x1 / (x2 - x1)
+        return self.k, self.b
+
+    def solve(self, x, k, b):
+        self.f = k * x + b
+        return self.f
 
 
 class DataParcer:
@@ -232,6 +408,7 @@ class DataParcer:
 
 def main():
     global nb
+
     if os.path.exists(r"entry_data/config.txt"):
         print('config exist')
     else:
@@ -250,33 +427,27 @@ def main():
 
     if not os.path.exists(r'time functions'):
         os.mkdir('time functions')
+    if not os.path.exists(os.path.join(cur_dir[0], 'time functions/user configuration')):
+        os.mkdir(os.path.join(cur_dir[0], 'time functions/user configuration'))
+
+    try:
+        lay_dir = os.path.join(cur_dir[0], 'entry_data/KUVSH.LAY')
+        DataParcer(lay_dir).lay_decoder()
+    except FileNotFoundError:
+        mb.showerror('Path error', 'Директория указана неверно')
+        cur_dir.clear()
+        open_button()
+        cur_dir = config_read()
 
     lay_dir = os.path.join(cur_dir[0], 'entry_data/KUVSH.LAY')
     pl_dir = os.path.join(cur_dir[0], 'entry_data/KUVSH.PL')
     tok_dir = os.path.join(cur_dir[0], 'entry_data/KUVSH.TOK')
     grid_dir = os.path.join(cur_dir[0], 'entry_data/KUVSH.GRD')
-    # print('TOK\n',DataParcer(tok_dir).tok_decoder())
-    # print('LAY\n', DataParcer(lay_dir).lay_decoder())
-    # print('PL\n', DataParcer(pl_dir).pl_decoder())
 
-    # print('NON zero PL\n', np.nonzero(DataParcer(pl_dir).pl_decoder()))
-    # print('Count non zero PL  = ', np.count_nonzero(DataParcer(pl_dir).pl_decoder()))
-    # print('Count non zero LAY = ', np.count_nonzero(DataParcer(lay_dir).lay_decoder()[:, 1:]))
-    # print('Count non zero TOK = ', np.count_nonzero(DataParcer(tok_dir).tok_decoder()))
-
-    # books_pl = np.count_nonzero(DataParcer(pl_dir).pl_decoder())
-    # books_tok = np.count_nonzero(DataParcer(tok_dir).tok_decoder())
-    # books_lay = np.count_nonzero(DataParcer(lay_dir).lay_decoder()[:, 1:])
-    # books_count = sum((np.count_nonzero(DataParcer(pl_dir).pl_decoder()),
-    #                    np.count_nonzero(DataParcer(tok_dir).tok_decoder()),
-    #                    np.count_nonzero(DataParcer(lay_dir).lay_decoder()[:, 1:])))
-    # print('sum = ', books_count)
-    # a = np.nonzero(DataParcer(pl_dir).pl_decoder())
-
-    print(DataParcer(grid_dir).grid_parcer().shape)
-    print(FrameGen(root).time_grid())
+    # print(DataParcer(grid_dir).grid_parcer().shape)
+    # print(FrameGen(root).time_grid())
     nb = ttk.Notebook(root)
-    nb.grid(row=5, column=0, columnspan=10, rowspan=10)
+    nb.grid(row=0, column=0, columnspan=100, rowspan=100)
     LAY = DataParcer(lay_dir).lay_decoder()
     PL = DataParcer(pl_dir).pl_decoder()
     TOK = DataParcer(tok_dir).tok_decoder()
@@ -305,7 +476,7 @@ def main():
 
 if __name__ == '__main__':
     root = tk.Tk()
-    root.geometry('800x600+300+200')
+    root.geometry('1200x600')
 
     main()
 
--- time func.py
+++ time func.py
@@ -2,9 +2,10 @@ from tkinter import *
 from tkinter import ttk
 import numpy as np
 import matplotlib.pyplot as plt
+import numpy.random
 
 
-class Func:
+class Calculations:
     def __init__(self):
         self.out = np.empty(1)
         self.b = 0
@@ -12,7 +13,7 @@ class Func:
 
     def grid_parcer(self):
         #### .PL DECODER
-        with open(r'C:\Users\Никита\Dropbox\work_cloud\source_cont\entry_data\KUVSH.GRD', 'r') as file:
+        with open(r'C:\Users\Zalozhnyy_N\Dropbox\work_cloud\source_cont\entry_data\KUVSH.GRD', 'r') as file:
             lines = file.readlines()
         self.out = np.array(lines[15].split(), dtype=float)
         return self.out
@@ -29,9 +30,16 @@ def solve(x, k, b):
 
 
 if __name__ == '__main__':
-    entry_f = np.array((1, 0.8, 0.6, 0.4, 0.2, 0))
-    entry_t = np.array((1e-10, 1e-9, 2e-9, 5e-9, 1e-8, 1e-7))
-    time_cell = Func().grid_parcer()
+    time_cell = Calculations().grid_parcer()
+    entry_f = np.array((1, 0.5, 0.3))
+    entry_t = np.array((1e-10, 2e-9, 1e-7))
+    # тесты интерполяции путём рандомизации времени
+    # n = 10
+    # entry_f = numpy.random.random_sample(n)
+    # entry_t = np.sort(numpy.random.random_sample(n) * time_cell[-1])
+
+    # print(entry_f)
+    # print(entry_t)
 
     if entry_t[-1] != time_cell[-1]:
         entry_t = np.append(entry_t, time_cell[-1])
@@ -41,21 +49,10 @@ if __name__ == '__main__':
         entry_f = np.insert(entry_f, 0, 0)
 
     for i in range(len(entry_t) - 1):
-        if entry_t[i] < entry_t[i + 1]:
-            print(f'Value error время уменьшается на одном из отрезков')
+        if entry_t[i] > entry_t[i + 1]:
+            print(f'Value error время уменьшается на одном из отрезков {i}')
 
     old_tf = np.loadtxt(r'entry_data\time.tf', skiprows=3)
-
-    print(f'entry_t before = {entry_t}')
-    # for i in range(len(entry_t)):
-    #     local_min = np.ones(len(time_cell))
-    #     for j in range(len(time_cell)):
-    #         # print(f'{time_cell[j]} {entry_t[i]} {time_cell[j + 1]}')
-    #
-    #         local_min[j] = abs(time_cell[j] - entry_t[i])
-    #     print(local_min)
-    #     print('index = ', np.argmin(local_min))
-    #     print((np.min(local_min)))
 
     print(f'entry_t = {entry_t}')
     print(f'entry_f = {entry_f}')
@@ -63,11 +60,18 @@ if __name__ == '__main__':
     time_count = []
     func_out = []
     for i in range(len(entry_t) - 1):
-        k, b = Func().linear_dif(entry_f[i], entry_t[i], entry_f[i + 1], entry_t[i + 1])
-        # print(f'k = {k} , b = {b}')
+        k, b = Calculations().linear_dif(entry_f[i], entry_t[i], entry_f[i + 1], entry_t[i + 1])
+        print(f'k = {k} , b = {b}')
         # print(np.extract((time_cell == entry_t[i]),time_cell))
+        dt = time_cell[1] - time_cell[0]
         left_side = np.where(time_cell == entry_t[i])[0]
         right_side = np.where(time_cell == entry_t[i + 1])[0]
+
+        if len(left_side) != 1:
+            left_side = np.where(abs(time_cell - entry_t[i]) <= dt / 2)[0]
+        if len(right_side) != 1:
+            right_side = np.where(abs(time_cell - entry_t[i + 1]) <= dt / 2)[0]
+
         # print(type(left_side), time_cell[left_side])
         # print(type(right_side), time_cell[right_side][0], time_cell[100])
         print(f'{time_cell[left_side]} - {time_cell[right_side]}')
@@ -87,15 +91,17 @@ if __name__ == '__main__':
 
     func_out = np.array(func_out)
     time_count = np.array(time_count)
-
+    output_matrix = np.column_stack((time_count, func_out))
     print(func_out.shape)
     print('заданныйэ = ', time_cell.shape, time_cell[-1])
     print('по факту = ', time_count.shape, time_count[-1])
     print(old_tf.shape)
 
-    np.savetxt('1.txt', (np.c_[time_count]))
-    np.savetxt('2.txt', (np.c_[time_cell]))
-    plt.plot(time_count, func_out * np.max(old_tf[:, 1]))
-    plt.plot(time_count, old_tf[:, 1])
+    np.savetxt('time functions/time_fixed.tf', output_matrix, fmt='%-8.4g',
+               header=f'1 pechs\n{time_count[0]} {time_count[-1]} {1.0}\n{len(time_count)}\n', delimiter='\t',
+               comments='')
 
+    plt.plot(time_count, func_out * np.max(old_tf[:, 1]), label='Пользовательская функция')
+    plt.plot(time_count, old_tf[:, 1], label='Стандартная функция')
+    plt.legend()
     plt.show()

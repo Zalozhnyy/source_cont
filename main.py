9fb98f755f091fb897d8e3745c4efca06083f068
Nick <niczz9797@gmail.com>
2020-03-07 13:19:59 +0300

    ВВод гурсы - всё плохо

--- main.py
+++ main.py
@@ -61,6 +61,9 @@ def check_folder():
 
 
 class FrameGen(tk.Frame):
+    # def __new__(cls, parent, name='Title', energy_type='Название типа энергии'):
+    #     return super(FrameGen, cls).__new__(cls)
+
     def __init__(self, parent, name='Title', energy_type='Название типа энергии'):
         tk.Frame.__init__(self, parent)
         self.parent = root
@@ -77,6 +80,12 @@ class FrameGen(tk.Frame):
 
         self.path = os.path.normpath(config_read()[0])
         self.dir_name = config_read()[0].split('/')[-1]
+        self.gursa_count = 0
+        self.label_util = []
+        print(repr(self))
+
+    def __repr__(self):
+        return f'{self.name}'
 
     def __konstr(self):
         self.parent.title("Sources")
@@ -91,11 +100,12 @@ class FrameGen(tk.Frame):
 
     def notebooks(self):
         nb.add(self, text=f"{self.name}")
-        # rows = 0
-        # while rows < 50:
-        #     self.rowconfigure(rows, weight=1,minsize=3)
-        #     self.columnconfigure(rows, weight=1,minsize=3)
-        #     rows += 1
+
+        rows = 0
+        while rows < 50:
+            self.rowconfigure(rows, weight=0, minsize=3)
+            self.columnconfigure(rows, weight=0, minsize=3)
+            rows += 1
         label_name_energy = tk.Label(self, text=f'{self.energy_type}')
         label_name_energy.grid(row=5, column=0, columnspan=2)
         label_func = tk.Label(self, text='value', width=8)
@@ -153,10 +163,26 @@ class FrameGen(tk.Frame):
                 tk.Button(self, text='Browse tf', command=lambda: print(123),
                           width=10, ).grid(row=2 + i, column=32, padx=5)
 
-    def gursa_nb(self):
-        nb.add(self, text=f"{self.name}")
-        add_button_gursa = tk.Button(self, text='add',
-                                     command=lambda: Gursa().child_window()).grid(row=1, column=1)
+        if 'Gursa' in self.name:
+            add_button_gursa = tk.Button(self, text='add', width=10, command=self.gursa_cw)
+            add_button_gursa.grid(row=10, column=2)
+
+    def gursa_cw(self):
+
+        try:
+            x = Gursa(name=f'Gursa {self.gursa_count}')
+            self.gursa_count += 1
+
+            self.label_util.append(x.name)
+            print(self.label_util)
+
+            for i in range(len(self.label_util)):
+                existe_gursa_label = tk.Label(self, text=f'{self.label_util[i]} / {x} / {repr(x)}')
+                existe_gursa_label.grid(row=11 + i, column=2)
+
+
+        except:
+            pass
 
     def ent(self):
         self.func_entry_vel.clear()
@@ -420,68 +446,79 @@ class FrameGen(tk.Frame):
 
 
 class Gursa(tk.Toplevel):
-    def __init__(self):
-        tk.Toplevel.__init__(self, root)
-        self.spectr_type = tk.IntVar()
+    def __init__(self, name='Gursa_N'):
+        super().__init__(root)
+
+        self.name = name
+        self.spectr = []
+        self.spectr_cont = []
+        self.Energy0 = []
+        self.EnergyP = []
         self.spectr_path = ''
+        self.Spektr_output = []
+
+        self.child_window()
+        print(repr(self))
+
+    # def __repr__(self):
+    #     return f'Gursa n{self.gursa_count}'
 
     def child_window(self):
         self.title('PE source')
         self.geometry('800x600')
+        self.grab_set()
+        self.focus()
 
         rows = 0
         while rows < 30:
             self.rowconfigure(rows, weight=0, minsize=3)
             self.columnconfigure(rows, weight=0, minsize=3)
             rows += 1
-        print(self.grid_size())
 
         x_y_z = ['X', 'Y', 'Z']
         label_names = ['Координаты источника (км)', 'Координаты объекта (км)', 'Число ячеек сетки']
-        entry_koord_ist_val = [tk.StringVar() for _ in range(3)]
-        entry_koord_obj_val = [tk.StringVar() for _ in range(3)]
-        entry_koord_ist_val[0].set('0')
-        entry_koord_ist_val[1].set('0')
-        entry_koord_ist_val[2].set('40')
-        entry_koord_obj_val[0].set('0')
-        entry_koord_obj_val[1].set('0')
-        entry_koord_obj_val[2].set('40')
-        entry_grid_value = tk.StringVar()
-        entry_grid_value.set('1000')
-        tk.Entry(self, textvariable=entry_grid_value, width=9).grid(row=3, column=1)
+        self.entry_koord_ist_val = [tk.StringVar() for _ in range(3)]
+        self.entry_koord_obj_val = [tk.StringVar() for _ in range(3)]
+        self.entry_koord_ist_val[0].set('0')
+        self.entry_koord_ist_val[1].set('0')
+        self.entry_koord_ist_val[2].set('40')
+        self.entry_koord_obj_val[0].set('0')
+        self.entry_koord_obj_val[1].set('0')
+        self.entry_koord_obj_val[2].set('60')
+        self.entry_grid_value = tk.StringVar()
+        self.entry_grid_value.set('1000')
+        tk.Entry(self, textvariable=self.entry_grid_value, width=9).grid(row=3, column=1)
 
         for i in range(3):
-            entry_koord_ist = tk.Entry(self, textvariable=entry_koord_ist_val[i], width=9)
+            entry_koord_ist = tk.Entry(self, textvariable=self.entry_koord_ist_val[i], width=9)
             entry_koord_ist.grid(row=1, column=1 + i, padx=1, pady=2)
             tk.Label(self, text=x_y_z[i]).grid(row=0, column=1 + i)
             tk.Label(self, text=label_names[i]).grid(row=1 + i, column=0, sticky='W')
         for i in range(3):
-            entry_koord_ist = tk.Entry(self, textvariable=entry_koord_obj_val[i], width=9)
+            entry_koord_ist = tk.Entry(self, textvariable=self.entry_koord_obj_val[i], width=9)
             entry_koord_ist.grid(row=2, column=1 + i, padx=1, pady=2)
 
         self.button_browse_spectr = tk.Button(self, text='Browse',
                                               command=self.take_spectr, state='normal')
         self.button_browse_spectr.grid(row=1, column=5, padx=5)
-        self.button_rasch = tk.Button(self, text='Calc',
-                                      command=lambda: print('calculated!'), state='disabled')
+        self.button_rasch = tk.Button(self, text='Calc', command=self.main, state='normal')
         self.button_rasch.grid(row=5, column=0)
 
-        self.spectr_type.set(0)
+        self.spectr_type = tk.IntVar()
+
         radio_cont = tk.Radiobutton(self, text='CONTINUOUS', variable=self.spectr_type, value=0)
         radio_cont.grid(row=1, column=6, sticky="W")
         radio_dis = tk.Radiobutton(self, text='DISCRETE', variable=self.spectr_type, value=1)
         radio_dis.grid(row=2, column=6, sticky="W")
-
-        self.grab_set()
-        self.focus_set()
+        self.spectr_type.set(0)
 
     def take_spectr(self):
-        path = fd.askopenfilename(title='Выберите файл spectr',
-                                  filetypes=(("all files", "*.*"), ("txt files", "*.txt*")))
+        # path = fd.askopenfilename(title='Выберите файл spectr',
+        #                           filetypes=(("all files", "*.*"), ("txt files", "*.txt*")))
+        path = 'spectr_3_49_norm_na_1.txt'
         with open(path, 'r') as file_handler:
             i = 0
             s = []
-
             if self.spectr_type.get() == 0:
                 for line in file_handler:
                     i += 1
@@ -497,6 +534,7 @@ class Gursa(tk.Toplevel):
                         s.append(line.split())
 
                 out = np.array(s, dtype=float)
+                self.spectr_cont = np.copy(out)
                 for i in range(len(out) - 1):
                     out[i, 0] = (out[i, 0] + out[i + 1, 0]) / 2
                     out[i, 1] = out[i + 1, 1]
@@ -507,8 +545,141 @@ class Gursa(tk.Toplevel):
                         mb.showerror('Spectr error', 'Выбран спектр CONTINUOUS')
                         break
                 out = np.loadtxt(path, skiprows=2)
-        return print(out)
 
+            self.button_rasch.configure(state='normal')
+            self.spectr = out
+        # print(self.spectr)
+
+    def delta_R(self, DOT0, DOT1):
+        r = ((DOT0[0] - DOT1[0]) ** 2 + (DOT0[1] - DOT1[1]) ** 2 + (DOT0[2] - DOT1[2]) ** 2) ** 0.5
+        return r
+
+    def ro_air(self, h, h0):
+        ro = 1.23 * 10 ** -3 * np.exp(-(h + h0) / 7)
+        return ro
+
+    def koord(self, x1, y1, z1):
+        A = np.array((x1, y1, z1), dtype=float)
+        return A
+
+    def koord_param(self, x1, x2, t):  # D1-источник  D2- короббка
+        x = x1 + (x2 - x1) * t
+        return x
+
+    def main(self):
+        print(repr(self))
+
+        M2 = self.koord(self.entry_koord_ist_val[0].get(),
+                        self.entry_koord_ist_val[1].get(),
+                        self.entry_koord_ist_val[2].get())
+        M1 = self.koord(self.entry_koord_obj_val[0].get(),
+                        self.entry_koord_obj_val[1].get(),
+                        self.entry_koord_obj_val[2].get())
+        N = int(self.entry_grid_value.get())
+
+        self.Energy0 = self.spectr[:, 0] * 10 ** -3  # энергия должна быть в МэВ
+        self.EnergyP = self.spectr[:, 1]
+
+        self.Spektr_output = np.zeros((len(self.Energy0), 2), dtype=float)
+
+        # БЛОК ЧТЕНИЯ ТАБЛИЦ ПОСТОЯННЫХ
+        materials_path = os.path.join(config_read()[0], 'pechs\materials')
+        if os.path.exists(materials_path):
+            print(f'{materials_path} exist')
+
+        photon_values_dir = os.path.join(materials_path, 'mat-air/photon/xtbl.23')
+        electron_values_dir = os.path.join(materials_path, 'mat-air/electron/xtbl.23')
+
+        electron_values = np.loadtxt(electron_values_dir, skiprows=18)
+        photon_values = np.loadtxt(photon_values_dir, skiprows=18)
+
+        electron_values = 10 ** electron_values
+        photon_values = 10 ** photon_values
+
+        sum_dol = np.zeros((len(self.Energy0), N), dtype=float)
+
+        for j in range(len(self.Energy0)):
+
+            Lx = M1[0] - M2[0]
+            Ly = M1[1] - M2[1]
+            Lz = M1[2] - M2[2]
+            dx = Lx / N
+            dy = Ly / N
+            dz = Lz / N
+            PT = np.linspace(0, 1, N)
+
+            R0 = ((M1[0] - M2[0]) ** 2 + (M1[1] - M2[1]) ** 2 + (M1[2] - M2[2]) ** 2) ** 0.5
+
+            X = np.zeros(N, dtype=float)
+            Y = np.zeros(N, dtype=float)
+            Z = np.zeros(N, dtype=float)
+            R = np.zeros(N, dtype=float)
+            ro = np.zeros(N, dtype=float)
+
+            for i in range(len(X)):
+                X[i] = self.koord_param(M2[0], M1[0], PT[i])
+                Y[i] = self.koord_param(M2[1], M1[1], PT[i])
+                Z[i] = self.koord_param(M2[2], M1[2], PT[i])
+                pr_k = np.array((X[i], Y[i], Z[i]))
+                R[i] = self.delta_R(M2, pr_k)
+
+            for i in range(N):  # расчет плотности
+                if i == 0:
+                    ro[i] = self.ro_air(0, M2[2])
+                else:
+                    if M2[2] > M1[2]:
+                        ro[i] = self.ro_air(0, Z[i])
+                    else:
+                        ro[i] = self.ro_air(Z[i] - Z[0], M2[2])
+
+            Energy_reduction = np.zeros(N, dtype=float)
+            lam = np.zeros(N, dtype=float)
+            Energy_reduction[0] = self.EnergyP[j]
+            sum_dol[j, 0] = self.EnergyP[j]
+
+            KSI = np.interp(self.Energy0[j] * 10 ** 6, photon_values[:, 0], photon_values[:, 1])
+
+            for i in range(N):
+                lam[i] = KSI * ro[i]  # cm**2/g * g/cm**3 ------- 1/cm
+            for i in range(1, N):
+                Energy_reduction[i] = Energy_reduction[i - 1] * np.exp(
+                    -(R[i] * 10 ** 5 - R[i - 1] * 10 ** 5) * lam[i])
+                sum_dol[j, i] = Energy_reduction[i]
+
+            self.Spektr_output[j, 1] = Energy_reduction[-1] * (4 * np.pi * (R0 * 10 ** 5) ** 2) ** -1
+
+        sum_dol_out = sum_dol.sum(axis=0)
+
+        for i in range(1, N):
+            sum_dol_out[i] = sum_dol_out[i] * (4 * np.pi * (R[i] * 10 ** 5) ** 2) ** -1
+
+        if self.spectr_type.get() == 1:
+            type = 'DISCRETE'
+        elif self.spectr_type.get() == 0:
+            type = 'CONTINUOUS'
+
+        if self.spectr_type.get() == 1:
+            self.Spektr_output[:, 0] = self.Energy0
+            np.savetxt(f'time functions/{gursa_class_nb.dir_name}/TOK/Spektr_output_{gursa_class_nb.name}_1.txt',
+                       self.Spektr_output, fmt='%-6.3g', header='SP_TYPE={}\n[DATA]'.format(type),
+                       comments='', delimiter='\t')
+        elif self.spectr_type.get() == 0:
+            self.Spektr_output[:, 0] = self.spectr_cont[1:, 0]
+            for i in range(len(self.Spektr_output) - 1, 0, -1):
+                self.Spektr_output[i, 1] = self.Spektr_output[i - 1, 1]
+
+            np.savetxt(f'time functions/{gursa_class_nb.dir_name}/TOK/Spektr_output_{gursa_class_nb.name}_1.txt',
+                       self.Spektr_output, fmt='%-6.3g', comments='', delimiter='\t',
+                       header='SP_TYPE={}\n[DATA]\n{:.2g}'.format(type, self.spectr_cont[0, 0]))
+
+        self.destroy()
+
+    # def name_transfer(self):
+    #     if self.spectr_type.get() == 1:
+    #         type = 'DISCRETE'
+    #     elif self.spectr_type.get() == 0:
+    #         type = 'CONTINUOUS'
+    #     return type
 
 
 class Calculations:
@@ -585,6 +756,7 @@ class DataParcer:
 
 
 def checker():
+    check_class = FrameGen(root, 'Check')
     if os.path.exists(r"config.txt"):
         print('config exist')
     else:
@@ -605,14 +777,14 @@ def checker():
 
     if not os.path.exists(r'time functions'):
         os.mkdir('time functions')
-    if not os.path.exists(f'time functions/{FrameGen(root).dir_name}'):
-        os.mkdir(f'time functions/{FrameGen(root).dir_name}')
-    if not os.path.exists(f'time functions/{FrameGen(root).dir_name}/user configuration'):
-        os.mkdir(os.path.join(f'time functions/{FrameGen(root).dir_name}/user configuration'))
+    if not os.path.exists(f'time functions/{check_class.dir_name}'):
+        os.mkdir(f'time functions/{check_class.dir_name}')
+    if not os.path.exists(f'time functions/{check_class.dir_name}/user configuration'):
+        os.mkdir(os.path.join(f'time functions/{check_class.dir_name}/user configuration'))
 
     if len(DataParcer(os.path.normpath(os.path.join(cur_dir[0], check_folder().get('TOK')))).tok_decoder()) > 0:
-        if not os.path.exists(f'time functions/{FrameGen(root).dir_name}/TOK'):
-            os.mkdir(os.path.join(f'time functions/{FrameGen(root).dir_name}/TOK'))
+        if not os.path.exists(f'time functions/{check_class.dir_name}/TOK'):
+            os.mkdir(os.path.join(f'time functions/{check_class.dir_name}/TOK'))
 
     try:
         lay_dir = os.path.join(cur_dir[0], check_folder().get('LAY'))
@@ -623,12 +795,13 @@ def checker():
         open_button()
         cur_dir = config_read()
     # check_folder()
+    del check_class
 
     return cur_dir
 
 
 def main():
-    global nb
+    global nb, gursa_class_nb
 
     cur_dir = checker()
     file_dict = check_folder()
@@ -668,7 +841,8 @@ def main():
         FrameGen(root, 'External_field', f'{energy_type}').notebooks()
     if TOK[2] == 1:
         energy_type = 'Gursa'
-        FrameGen(root, 'Gursa', f'{energy_type}').gursa_nb()
+        gursa_class_nb = FrameGen(root, 'Gursa', f'{energy_type}')
+        gursa_class_nb.notebooks()
 
 
 if __name__ == '__main__':
@@ -679,7 +853,6 @@ if __name__ == '__main__':
         root.rowconfigure(rows, weight=1)
         root.columnconfigure(rows, weight=1)
         rows += 1
-    print()
     main()
 
     root.mainloop()
